816e318989e954836233f2d7ccd7083613948c52
BradMarx <bradmarx@aol.com>
2014-05-18 00:01:31 -0500

    My Text Adventure

---
+++ TextAdventure.java
@@ -0,0 +1,32 @@
+public class TextAdventure
+{
+public static void main(String Args)
+{
+private string finished = "no";
+Scanner key = new Scanner(System.in);
+System.out.println("Would you like to play a game?");
+finished = key.nextString();
+if(finished == "no")
+{//just a check to see if person wants to play or not
+while(finished == "no")
+{
+System.out.println("Welcome to the last place you will ever call the place that you are at for the time being,");
+System.out.println("You are in a dangerous place where any wrong move can lead to a painful miserable death.");
+System.out.println("You must be careful and make choices with smartness, or you can just kill yourself now.");
+System.out.println("");//To help separate the question from the information, this is not designed to be a story driven game, just nested if statement testing
+System.out.println("Do you want to kill yourself now");
+finished = key.nextString();
+if(finished == "no")
+{
+System.out.println("");
+}
+else
+{
+System.out.println("A rock appeared next to you. It floated up and above your head, then dropped. You be dead"); 
+}
+}
+}
+else
+System.out.println("That's OK, you can always play later");
+}
+}
